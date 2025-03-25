f77075c378b6b2d8dfff5d4b9d42d4cbae1118e3
Colauncha <colauncha@gmail.com>
2025-03-25 23:26:22 +0000

    Sync from organization repo

---
+++ personal-repo/personal-repo/personal-repo/personal-repo/src/Pages/Sell/List.jsx
@@ -0,0 +1,9 @@
+import { useNavigate } from "react-router-dom";
+
+const List = () => {
+  // const navigate = useNavigate();
+  // navigate("/list");
+  return <div></div>;
+};
+
+export default List;
